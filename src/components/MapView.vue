b788cb80e827739a2e5ea67084e40b6fa849a512
Wei Khiang <weikhiang92000@gmail.com>
2022-10-18 02:21:37 +0800

    route card to mapview and display restaurant based on url params

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,13 +1,63 @@
-<template lang="">
-  <div>
-    hello world
-  </div>
+<template>
+  <GMapMap
+      :center="center"
+      :zoom="17"
+      map-type-id="terrain"
+      style="width: 100vw; height: 100vh"
+  >
+    <GMapCluster>
+      <GMapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="center=m.position"
+      />
+    </GMapCluster>
+  </GMapMap>
 </template>
-<script>
-export default {
 
+<script>
+
+export default {
+  name: 'MapView',
+
+  data() {
+    return {
+      center: {lat: 1.296568, lng: 103.852119},
+      markers: [
+        {
+          position: {
+            lat: 1.296568, lng: 103.852119
+          },
+        }
+        , // Along list of clusters
+      ]
+    }
+  },
+  created() {
+    navigator.geolocation.watchPosition(
+      (pos) => {
+        this.center = {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+        }
+      },
+      (err) => {
+        console.log(err);
+      }
+    )
+
+    const lat = Number(this.$route.params.lat);
+    const lng = Number(this.$route.params.lng);
+    this.markers = [
+      {
+        position: {
+          lat: lat, lng: lng
+        },
+      }
+    ]
+  },
 }
 </script>
-<style lang="">
-  
-</style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 // import './assets/main.css'
+import VueGoogleMaps from '@fawmi/vue-google-maps'
 
 const app = createApp(App)
 
 app.use(router)
 
-
+app.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyDJx4A_FqpmuCF40Tm-gs4F0Z3we45UH6c',
+    },
+})
 
 app.mount('#app')
 import "bootstrap/dist/js/bootstrap.js";
