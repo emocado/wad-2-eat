a4418eadc6f9535d0569c71783276143d6164dd4
Wei Khiang <weikhiang92000@gmail.com>
2022-10-18 00:29:53 +0800

    move static data to data.js and set up structure for vue-google-maps

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ForumView from '../views/ForumView.vue'
 import GameCardsStackComponent from "../components/GameCardsStackComponent.vue";
+import MapView from "../components/MapView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
