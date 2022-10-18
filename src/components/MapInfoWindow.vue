2ee09d6a93e57eaab16a263179e96d5b0df2a77a
Wei Khiang <weikhiang92000@gmail.com>
2022-10-18 20:34:59 +0800

    create MapInfoWindow

---
+++ src/components/MapInfoWindow.vue
@@ -0,0 +1,45 @@
+<template lang="">
+    <div>
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    lat: {
+      type: Number,
+      required: true
+    },
+    lng: {
+      type: Number,
+      required: true
+    },
+  },
+
+  data: () => ({
+    infoW: null,
+  }),
+
+  mounted() {
+    this.$parent.getMap(map => {
+      this.infoW = new window.google.maps.InfoWindow({
+        content: this.$el,
+        position: {
+          lat: this.lat,
+          lng: this.lng,
+        },
+        disableAutoPan: true,
+      })
+      this.infoW.open(map)
+    })
+  },
+
+  beforeUnmount() {
+    this.infoW.close()
+  },
+
+}
+</script>
+<style lang="">
+    
+</style>
