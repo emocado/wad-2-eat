ce80b5dff6f6bb0dda4f9b44999006705c53c06d
Zuyao <zuyao.liu.2020@business.smu.edu.sg>
2022-11-13 03:22:10 +0800

    Added CSS for recipe search

--- src/components/MealCard.vue
+++ src/components/MealCard.vue
@@ -1,105 +1,21 @@
-<!-- <template>
-
-    <section class="meals">
-        <div v-for="(meal,index) in meals" :key="index">
-
-            <h1>{{meal.strMeal}}</h1>
-            <div class="meal_image">
-                <img :src="meal.strMealThumb" :alt="meal.strMealThumb">
-            </div>
-
-            <div class="meal_paragraph">
-                <p class="sub"> {{meal.strInstructions.substring(0,50)}}</p>
-                <p class="block"> {{meal.strInstructions}} </p>
-                <button @click="goTodetail(meal.strMealThumb)" >View More</button>
-                <router-link to="/RecipeInfo">link</router-link>
-            </div>
-        </div>
-
-    </section>
-
-
-</template>
-    
-    
-<script>
-import { VueElement } from 'vue';
-import RecipeInfo from './RecipeInfo.vue';
-export default {
-    props: ['meals'],
-    methods: {
-        goTodetail(food){
-            this.$router.addRoute({ name: 'RecipeInfo', path: '/RecipeInfo', component: RecipeInfo })
-            console.log(meal)
-            // window.location.replace("/recipe/RecipeInfo")
-
-            
-
-        }
-    }
-
-}
-</script>
-    
-<style scoped>
-* {
-    font-family: 'Roboto Mono', monospace;
-}
-
-.meals {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 1em;
-    justify-content: center;
-    background: #ecf0f3;
-    padding: 50px 10px;
-    border-radius: 10px;
-}
-
-.meal_paragraph {
-    padding: 10px 30px;
-}
-
-.meals>* {
-    flex-basis: 30%;
-    min-width: 10rem;
-    border: 1px solid #000;
-    border-radius: 20px;
-    box-shadow: -3px -3px 7px #fff, 3px 3px 5px #ceced1;
-    text-align: center;
-}
-
-img {
-    max-width: 100%;
-    border-radius: 20px;
-}
-
-.meal_image {
-    padding: 3px 50px;
-}
-
-.block {
-    display: none;
-}
-</style> -->
-
-
 <template>
 
     <section class="meals">
         <div v-for="(meal, index) in meals" :key="index">
 
-            <h1>{{ meal.title }}</h1>
-            <div class="meal_image">
+            
+            <div class="meal_image" style="padding-top:20px">
                 <img :src="`https://spoonacular.com/recipeImages/` + meal.id + '-636x393.jpg'" :alt="meal.strMealThumb">
             </div>
+
+            <h5>{{ meal.title }}</h5>
 
             <div class="meal_paragraph">
                 <!-- <p class="sub"> {{meal.sourceURL.substring(0,50)}}</p> -->
                 <p class="block"> {{ meal.sourceURL }} </p>
                 <p>Servings: {{ meal.servings }}</p>
                 <p>Preparation Time: {{ meal.readyInMinutes }} Minutes </p>
-                <button @click="goTodetail(meal.id)">View More</button>
+                <button @click="goTodetail(meal.id)" style="color:grey;text-decoration: underline;">View More</button>
                 <!-- <router-link to="/RecipeInfo">link</router-link> -->
                 
             </div>
@@ -133,7 +49,7 @@ export default {
     
 <style scoped>
 * {
-    font-family: 'Roboto Mono', monospace;
+    font-family: 'Mali', monospace;
 }
 
 .meals {
@@ -141,16 +57,19 @@ export default {
     flex-wrap: wrap;
     gap: 1em;
     justify-content: center;
-    background: #ecf0f3;
+    /* background: #ecf0f3; */
+    
     padding: 50px 10px;
     border-radius: 10px;
 }
 
 .meal_paragraph {
     padding: 10px 30px;
+    color:black;
 }
 
 .meals>* {
+    background:#F3E0DC;
     flex-basis: 30%;
     min-width: 10rem;
     border: 1px solid #000;
--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -1,71 +1,39 @@
-<!-- <template>
-      
-    <div id="meals">
-        <input type="text" v-model="search" class="search">
-        <button @click="fetchData()">search</button>
-    
-        <MealCard :meals="meals"></MealCard>
-    </div>
-       
-    </template>
-    
-    <script>
-    import axios from  "axios"
-    import MealCard from "./MealCard.vue"
-    
-        export default{
-        
-        data() {
-            return {
-                search:"",
-                meals: []
-            };
-        },
-        methods: {
-            fetchData() {
-                axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${this.search}`)
-                    .then(res => {
-                    this.meals = res.data.meals;
-                    console.log(res.data);
-                    if(this.meals==null){
-                      
-                    }
-                }).catch(err => console.log(err));
-                
-            }
-        },
-        components: { MealCard }
-    }
-    </script>
-    <style scoped>
-       
-    input{
-        width: 20rem;
-        height: 2rem;
-        margin: 30px 10px;
-    }
-    button{
-        height: 2rem;
-    }
-    .loading{
-        display: grid;
-        place-content: center;
-    }
-    input:focus{
-        outline: 1px solid #669;
-    }
-    </style> -->
-    
-    
-
      <template>
-      
+            <head>
+        <title>Recipe Search</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
+        <link href="https://fonts.googleapis.com/css2?family=Mali:ital,wght@1,700&display=swap" rel="stylesheet">
+    </head>
+
+        <!-- Jumbotron -->
+       <div class="jumbotron jumbotron-fluid vertical-center text-center">
+            <div class="container">
+            <h1 class="display-4" style="color:white;">What's for Dinner?</h1>
+            <!-- <hr class="my-4"> -->
+            <p>It's the age-old question. Search to find out.</p>
+                <!-- Form for search field and button -->
+                <form class="form-check form-check-inline">
+                    <input class="form-control mr-sm-2" v-model="search" type="search" placeholder="Search for Food" aria-label="Search" id="search-input">
+                    <button @click="fetchData(search)" class="btn my-2 my-sm-0" type="button" id="search-button" ><i class="fas fa-search"></i></button>
+                </form>
+
+                
+                <MealCard :meals="meals"></MealCard>
+
+                
+            </div>
+            
+        </div>
+
+
+<!-- 
         <div id="meals">
             <input type="text" v-model="search" class="search">
             <button @click="fetchData(search)">search</button>
         
             <MealCard :meals="meals"></MealCard>
-        </div>
+        </div> -->
            
         </template>
         
@@ -87,6 +55,7 @@
                         .then(res => {
                         this.meals = res.data.results;
                         console.log(res.data.results)
+                        
                         if(this.meals==null){
                           
                         }
@@ -99,7 +68,137 @@
             components: { MealCard }
         }
         </script>
+
+
         <style scoped>
+
+body {
+    background-color: #D4A59A;
+    height: 100%;
+    text-align: center;
+}
+
+h1 {
+    font-family: 'Mali', cursive;
+    font-style: italic;
+    margin-bottom: 0px;
+}
+
+/* Jumbotron */
+.jumbotron {
+    background-color: #5C2018;
+    color: white;
+    padding: 30px 20px 40px 20px;
+    margin-bottom: 20px;
+    background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url("../assets/recipebackground.jpg");
+    background-size: cover;
+    /* background-position: center center; */
+    min-height: 100vh;
+}
+
+.vertical-center {
+    display: flex;
+    align-items: center;
+
+}
+
+#search-input, #search-button {
+    margin: 10px 0px 10px 0px;
+    
+}
+
+/* Style for random cards */
+.card {
+    background-color: #F3E0DC;
+    display:inline-block;
+    margin: 0px 10px 10px 10px;
+}
+.cropped {
+    width: 400px;
+    height: 230px;
+    object-fit: cover;
+    overflow: hidden;
+}
+.cropped img {
+    margin: 10px 0px 0px 0px;
+    width: 90%;
+}
+
+/* Search bar */
+.form-control {
+    background-color: #F3E0DC;
+    border: 1px solid #ffffff;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    border-top-left-radius: 30px;
+    border-bottom-left-radius: 30px;
+    margin: 0;
+    width: 300px;
+    height:48px;
+}
+
+/* search button */
+#search-button {
+    background-color: #5C2018;
+    color: white;
+    border-color: white;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    border-top-right-radius: 30px;
+    border-bottom-right-radius: 30px;
+    
+}
+
+#search-button:hover {
+    color: black;
+    border-color: black;
+}
+
+#search-button:active {
+    background-color: #5C2018;
+}
+
+@media (min-width: 576px) {
+    .mr-sm-2, .mx-sm-2 {
+        margin-right: 0px!important;
+    }
+}
+
+@media (max-width: 420px) {
+    .card {
+        width: 90%!important;
+    }
+    .cropped {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        overflow: hidden;
+    }
+    .cropped img {
+        margin: 10px 0px 0px 0px;
+        width: 90%;
+    }
+}
+
+/* Custom styling of radio button */
+.custom-control-input:checked ~ .custom-control-label::before {
+    color: #fff;
+    border-color: #5C2018;
+    background-color: #5C2018;
+}
+
+/* Custom styling of randomizer buttons */
+.btn-primary {
+    background-color: #5C2018;
+    border-color: #5C2018;
+}
+
+.btn-primary:hover {
+    background-color: #D4A59A;
+    border-color: #D4A59A;
+}
+
+
            
         input{
             width: 20rem;
